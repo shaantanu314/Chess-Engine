7689348dc51a1323582fe0ed8d7792242ea21d9d
shaantanu314 <shaantanu2018@gmail.com>
2020-12-22 00:55:50 +0530

    chess engine updated code

--- Chess_project/ChessMain.py
+++ Chess_project/ChessMain.py
@@ -4,7 +4,7 @@ import ChessEngine
 WIDTH  = 512
 HEIGHT = 512
 DIMENSION = 8
-SQ_SIZE = WIDTH//DIMENSION
+SQ_SIZE = 64
 MAX_FPS = 15
 IMAGES = {}
 
@@ -55,33 +55,55 @@ def main():
                 if len(playerClicks)==2:
 
                     move = ChessEngine.Move(playerClicks[0],playerClicks[1],gs.board)
-                    if move in validMoves:
-                        moveMade = True
-                        gs.makeMove(move)
 
-                    sqSelected = ()
-                    playerClicks = []
+                    for i in range(len(validMoves)):
+                        if move == validMoves[i]:
+                            moveMade = True
+                            gs.makeMove(validMoves[i])
+                            sqSelected = ()
+                            playerClicks = []
+                            if move.pawnPromotion:
+                                gs.board[move.endRow][move.endCol] = move.currPiece[0] + "Q"
+                                # CHOOSE THE PIECE FOR PAWN PROMOTION:
+                                
+                                # promote_to = input("SELECT ON OF THE FOLLOWING FOR PAWN PROMOTION:\n 1) Q for Queen \n 2) R for Rook\n 3) B for Bishop\n 4) N for Knight\n")
+                                # print(promote_to)
+                    if not moveMade :
+                        playerClicks = [sqSelected]
+                    
         if moveMade:
             validMoves = gs.getValidMoves()
             moveMade = False
-        drawGameState(screen,gs)
+        drawGameState(screen,gs,sqSelected,validMoves)
         clock.tick(MAX_FPS)
         pygame.display.flip()
 
-def drawGameState(screen,gs):
+def drawGameState(screen,gs,sqSelected,validMoves):
     # draws the Chess board
-    drawBoard(screen)
+    drawBoard(screen,sqSelected,validMoves)
     # draws pieces on the board according to 
     drawPieces(screen,gs.board)
 
 
 
-def drawBoard(screen):
+def drawBoard(screen,sqSelected,validMoves):
     colors = [pygame.Color("white"),pygame.Color("gray")]
     for r in range(DIMENSION):
         for c in range(DIMENSION):
             color = colors[(r+c)%2]
             pygame.draw.rect(screen,color,pygame.Rect(c*SQ_SIZE,r*SQ_SIZE,SQ_SIZE,SQ_SIZE))
+    
+    # Highlighting the seleted square effect
+    if len(sqSelected):
+        if (sqSelected[0]+sqSelected[1])%2 == 1:
+            pygame.draw.rect(screen,(211,205,71),pygame.Rect(sqSelected[1]*SQ_SIZE,sqSelected[0]*SQ_SIZE,SQ_SIZE,SQ_SIZE))
+        elif (sqSelected[0]+sqSelected[1])%2 == 0:
+            pygame.draw.rect(screen,(211,205,71),pygame.Rect(sqSelected[1]*SQ_SIZE,sqSelected[0]*SQ_SIZE,SQ_SIZE,SQ_SIZE))
+
+        # Highlighting all possible movesa for the selected square:    
+        for move in validMoves :
+            if (move.startRow , move.startCol ) == sqSelected :
+                pygame.draw.rect(screen,(26,232,60),pygame.Rect(move.endCol*SQ_SIZE+1,move.endRow*SQ_SIZE+1,SQ_SIZE-2,SQ_SIZE-2))
 
 def drawPieces(screen,board):
     for r in range(DIMENSION):
